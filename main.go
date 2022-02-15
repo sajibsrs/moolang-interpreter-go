59195f29a786b2f255a2319868546ef9a0286fe8
Sajidur Rahman <sajibsrs@gmail.com>
2022-02-16 05:39:21 +0600

    main

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"moolang/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! Welcome to moolang REPL!\n", user.Username)
+	fmt.Printf("Type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
