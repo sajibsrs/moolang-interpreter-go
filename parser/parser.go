6a70b4b52acb026a26ce17235cdfe7004a12c6d0
Sajidur Rahman <sajibsrs@gmail.com>
2022-02-17 18:40:19 +0600

    added: parser

---
+++ parser/parser.go
@@ -0,0 +1,105 @@
+package parser
+
+import (
+	"moolang/ast"
+	"moolang/lexer"
+	"moolang/token"
+)
+
+// Token parser
+type Parser struct {
+	lexer     *lexer.Lexer
+	errors    []string
+	curToken  token.Token
+	peekToken token.Token
+}
+
+// Creates new parser
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		lexer:  l,
+		errors: []string{},
+	}
+
+	// empty := token.Token{}
+
+	// for (p.curToken == empty) || (p.peekToken == empty) {
+	// 	p.nextToken()
+	// }
+
+	// Reads two token, so both curToken and peekToken are set
+	// p.nextToken()
+	// p.nextToken()
+
+	return p
+}
+
+// Move to next token
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.lexer.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO:
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
